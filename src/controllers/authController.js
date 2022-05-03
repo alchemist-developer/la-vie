5c096f6246c05b38c7d8acc77c8ce54d693d959e
Adryel Simon <adryel@live.it>
2022-05-03 10:46:57 -0300

    Sintaxe

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -14,11 +14,13 @@ const authController = {
     });
 
     if (!psicologo) {
-      return res.status(400).json("Usuário não Cadastrado");
+      return res.status(400).json("Usuário não Encontrado");
     }
 
     if (!bcrypt.compareSync(senha, psicologo.senha)) {
-      return res.status(401).json("E-mail ou senha inválida, verifique e tente novamente");
+      return res
+        .status(401)
+        .json("E-mail ou senha inválida, verifique e tente novamente");
     }
 
     const token = jwt.sign(
